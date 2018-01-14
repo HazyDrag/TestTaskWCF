b2aad2a75f972bbf8bec4f66bf0e15f1e1263020
Дмитрий <goldrag46@gmail.com>
2018-01-14 20:16:28 +0300

    MD5-hash passwords

--- AddressLibraryClient/MainWindow.xaml.cs
+++ AddressLibraryClient/MainWindow.xaml.cs
@@ -1,18 +1,38 @@
 ﻿using System;
+using System.Text;
 using System.Windows;
+using System.Security.Cryptography;
 
 using AddressLibraryClient.LibraryServiceReference;
 
 namespace AddressLibraryClient
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            helpLabel.Content = "Логин/пароль\ntestu/pass\nuser2/1111\nadmin/admin";
+        }
+
+        /// <summary>
+        /// Вычисление MD5
+        /// </summary>
+        /// <returns> MD5-hash строки </returns>
+        private static string MD5Hash(string input)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(input));
+            
+            byte[] result = md5.Hash;
+
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+                strBuilder.Append(result[i].ToString("x2"));
+
+            return strBuilder.ToString();
         }
 
         private void loginButton_Click(object sender, RoutedEventArgs e)
@@ -28,7 +48,7 @@ namespace AddressLibraryClient
                     
                     labelConnectionStatus.Content = "Соединение успешно\nПроверка данных";
 
-                    if (libClient.UserNameValidator(loginBox.Text, passwordBox.Password))
+                    if (libClient.UserNameValidator(loginBox.Text, MD5Hash(passwordBox.Password)))
                     {
                         labelConnectionStatus.Content = "УСПЕШНО!\nДобро пожаловать, " + loginBox.Text;
 
@@ -43,6 +63,7 @@ namespace AddressLibraryClient
                 {
                     // выводим информацию об ошибке
                     labelConnectionStatus.Content = "Ошибка: " + ex.Message;
+                    loginBox.Text = MD5Hash(passwordBox.Password);
                 }
             }
             loginButton.IsEnabled = true;
